b147dd3d6292b941aa0b43c002c8934dd12ac0a6
David Morrison <dm236@st-andrews.ac.uk>
2026-05-25 16:36:29 +0100

    chore: autoformatting

--- src/hproj/projectors/pca.py
+++ src/hproj/projectors/pca.py
@@ -6,15 +6,13 @@ from hproj.projectors.projector import Projector, ProjectorFactory
 
 @ProjectorFactory.register("pca")
 class PCAProjector(Projector):
-    def __init__(self, n_components, seed, **kwargs):
+    def __init__(self, n_components: int, seed: int, **kwargs):
         # PCA does not use the seed because there is no stochastic element
         self.n_components = n_components
         self.kwargs = kwargs
 
     def fit(self, space: FeatureSpace) -> None:
-        self.pca = PCA(
-            n_components=self.n_components, **self.kwargs
-        )
+        self.pca = PCA(n_components=self.n_components, **self.kwargs)
         self.pca.fit(space.features)
 
     def transform(self, space: FeatureSpace) -> FeatureSpace:
